4483559f8dfae2bf59a51a7a0bab707a60c52193
Chelobas14 <142736343+Chelobas14@users.noreply.github.com>
2024-08-10 17:56:12 +0600

    Add files via upload

--- funcs.js
+++ funcs.js
@@ -13,6 +13,10 @@ const vecMod = (vec) => {
     return Math.sqrt(vec.x**2 + vec.y**2)
 }
 
+const vecNorm = (vec) => {
+    return {x: vec.x / vecMod(vec), y: vec.y / vecMod(vec)}
+}
+
 const vecMultScalar = (vec, num) => {
     return {x: vec.x*num, y: vec.y*num}
 }
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const canv = document.querySelector("canvas");
 
-const w = 1600
+const w = 16000
 const h = 900
 
 canv.width = canv.style.width = w;
@@ -19,45 +19,87 @@ let params = {
 }
 
 let point = {
-  xStart: 200,
-  yStart: 0,
+  xStart: 400,
+  yStart: 500,
   x: 0,
   y: 0,
   powers: [],
   acceleration: {x: 0, y: 0},
   velocity: {x: 0, y: 0},
-  velocityStart: {x: 0, y: 50},
-  mass: 10**6
+  velocityStart: {x: -10, y: -230},
+  mass: 7 * 10**32
+}
+
+let point2 = {
+  xStart: 600,
+  yStart: 500,
+  x: 0,
+  y: 0,
+  powers: [],
+  acceleration: {x: 0, y: 0},
+  velocity: {x: 0, y: 0},
+  velocityStart: {x: 10, y: 230},
+  mass: 6 * 10**32
 }
 
 point.x = point.xStart;
 point.y = point.yStart;
 
+point2.x = point2.xStart;
+point2.y = point2.yStart;
+
+center = {x: 400, y: 600, mass: 10**33}
+
 const proc = setInterval(() => {
   time = timeInMs / 1000;
 
-  let tga = 1
-  let a = Math.atan(tga)
+  let gravityMod = params.G*point.mass*point2.mass / 
+  vecMod({x: point2.x - point.x, y: point2.y - point.y})**2/1000
+  let gravity = {x: 0, y: 0}
+  gravity.x = point2.x - point.x
+  gravity.y = point2.y - point.y
+  gravity = vecNorm(gravity)
+  gravity = vecMultScalar(
+    gravity, 
+    gravityMod
+  )
+
 
   point.powers =[
-    {
-      x: point.mass * 10**30 * params.G / (vecMod(point.y - 400, point.x - 400)*10**6) * Math.cos(a),
-      y: point.mass * 10**30 * params.G / (vecMod(point.y - 400, point.x - 400)*10**6) * Math.sin(a)
-    }
+    gravity
   ]
-
-  point.velocity.x = point.velocityStart.x + point.acceleration.x * time;
-  point.velocity.y = point.velocityStart.y + point.acceleration.y * time;
-
-  point.acceleration.x = vecSum(point.powers).x / point.mass;
+  point.acceleration.x = vecSum(point.powers).x / point.mass
   point.acceleration.y = vecSum(point.powers).y / point.mass
 
-  point.x = point.xStart + point.velocityStart.x * time + point.acceleration.x * time ** 2 / 2;
-  point.y = point.yStart + point.velocityStart.y * time + point.acceleration.y * time ** 2 / 2;
+  // point.velocity.x = point.velocityStart.x + point.acceleration.x * time;
+  // point.velocity.y = point.velocityStart.y + point.acceleration.y * time;
+
+  // point.x = point.xStart + point.velocityStart.x * time + point.acceleration.x * time ** 2 / 2;
+  // point.y = point.yStart + point.velocityStart.y * time + point.acceleration.y * time ** 2 / 2;
+
+  point.velocityStart.x += point.acceleration.x/1000*speed;
+  point.velocityStart.y += point.acceleration.y/1000*speed;
+
+  point.x += point.velocityStart.x/1000*speed;
+  point.y += point.velocityStart.y/1000*speed;
+
+  point2.powers =[
+    vecMultScalar(gravity, -1)
+  ]
+  point2.acceleration.x = vecSum(point2.powers).x / point2.mass
+  point2.acceleration.y = vecSum(point2.powers).y / point2.mass
+
+  point2.velocityStart.x += point2.acceleration.x/1000*speed;
+  point2.velocityStart.y += point2.acceleration.y/1000*speed;
+
+  point2.x += point2.velocityStart.x/1000*speed;
+  point2.y += point2.velocityStart.y/1000*speed;
+
   
   ctx.fillStyle = "red";
-  ctx.fillRect(point.x - 5, h - point.y + 5, 10, 10);
-  ctx.fillRect(400 - 5, h - 400 + 5, 10, 10);
+  ctx.fillRect(point.x - 2, h - point.y + 2, 4, 4);
+  ctx.fillStyle = "blue";
+  ctx.fillRect(point2.x - 2, h - point2.y + 2, 4, 4);
 
 
   if(timeInMs == 0){
